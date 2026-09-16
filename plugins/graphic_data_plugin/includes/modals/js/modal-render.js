94cfd2818019896075a6d9af747b2430659483ea
Robbie Carroll <47251604+robbiecarroll@users.noreply.github.com>
2026-09-16 14:57:03 -0400

    #567 fixes to the way interactive figures are saving based on new tab logic

--- plugins/graphic_data_plugin/includes/modals/js/modal-render.js
+++ plugins/graphic_data_plugin/includes/modals/js/modal-render.js
@@ -181,68 +181,100 @@ export function render_modal(key, obj, modal_obj){
                 tab_title = "No Tab Title Set";
             }
 
-            //Before we create tabs we are going to see which ones have figures and only create those by querying the restAPI.
-            //This step gets repeated in fetch_tab_info but with more parameters 
-            const protocol = window.location.protocol;
-            const host = window.location.host;
-            const fetchURL  =  protocol + "//" + host  + "/wp-json/wp/v2/figure?&per_page=24&order=asc&figure_modal=" + modal_id + "&figure_tab=" + i;
-            fetch(fetchURL)
-                .then(response => response.json())
-                .then(data => {
+            if (!window.location.href.includes('post.php') && !window.location.href.includes("post-new.php")) {
 
-                    let all_figure_data = data.filter(figure => Number(figure.figure_tab) === Number(i));
-                    all_figure_data = all_figure_data.filter(figure => Number(figure.figure_modal) === Number(modal_id) && String(figure.figure_published).toLowerCase() === 'published');
-                    //console.log('all_figure_data1', all_figure_data);
+                //Before we create tabs we are going to see which ones have figures and only create those by querying the restAPI.
+                //This step gets repeated in fetch_tab_info but with more parameters 
+                const protocol = window.location.protocol;
+                const host = window.location.host;
+                const fetchURL  =  protocol + "//" + host  + "/wp-json/wp/v2/figure?&per_page=24&order=asc&figure_modal=" + modal_id + "&figure_tab=" + i;
+                fetch(fetchURL)
+                    .then(response => response.json())
+                    .then(data => {
 
-                    //filter: If # of figures contained in the buttonID is > 0 generally & the number of figures = published is > 0 in the buttonID, show the tab.
-                    let total_published_figures = 0;
-                    for (let idx = 0; idx < all_figure_data.length; idx++) {
-                        const figure_data = all_figure_data[idx];
-                        const figure_published = figure_data['figure_published'];
-                        if (figure_published == "published") {
-                            total_published_figures += 1;
+                        let all_figure_data = data.filter(figure => Number(figure.figure_tab) === Number(i));
+                        all_figure_data = all_figure_data.filter(figure => Number(figure.figure_modal) === Number(modal_id) && String(figure.figure_published).toLowerCase() === 'published');
+                        //console.log('all_figure_data1', all_figure_data);
+
+                        //filter: If # of figures contained in the buttonID is > 0 generally & the number of figures = published is > 0 in the buttonID, show the tab.
+                        let total_published_figures = 0;
+                        for (let idx = 0; idx < all_figure_data.length; idx++) {
+                            const figure_data = all_figure_data[idx];
+                            const figure_published = figure_data['figure_published'];
+                            if (figure_published == "published") {
+                                total_published_figures += 1;
+                            }
                         }
-                    }
-                    //console.log('total_published_figures', total_published_figures);
+                        //console.log('total_published_figures', total_published_figures);
 
-                    //Do not create the tab if the tab has no published figures
-                    if (total_published_figures === 0) {
-                        return;
-                    }
+                        //Do not create the tab if the tab has no published figures
+                        if (total_published_figures === 0 && (!window.location.href.includes('post.php') && !window.location.href.includes("post-new.php"))) {
+                            return;
+                        }
 
-                    /*
-                    * Only the first tab encountered with published
-                    * figures receives true.
-                    */
-                    const is_first_tab_with_figures =
-                        !first_tab_with_figures_found;
+                        /*
+                        * Only the first tab encountered with published
+                        * figures receives true.
+                        */
+                        const is_first_tab_with_figures =
+                            !first_tab_with_figures_found;
 
-                    if (is_first_tab_with_figures) {
-                        first_tab_with_figures_found = true;
-                    }
+                        if (is_first_tab_with_figures) {
+                            first_tab_with_figures_found = true;
+                        }
 
-                    //console.log('is_first_tab_with_figures', is_first_tab_with_figures);
+                        // console.log('is_first_tab_with_figures', is_first_tab_with_figures);
 
-                    create_tabs(
-                        i,
-                        tab_key,
-                        tab_title,
-                        title,
-                        modal_id,
-                        is_first_tab_with_figures
-                    );
+                        create_tabs(
+                            i,
+                            tab_key,
+                            tab_title,
+                            title,
+                            modal_id,
+                            is_first_tab_with_figures
+                        );
 
-                    if (i === num_tabs) {
-                        let mdialog =
-                            document.querySelector(
-                                "#myModal > div"
-                            );
+                        if (i === num_tabs) {
+                            let mdialog =
+                                document.querySelector(
+                                    "#myModal > div"
+                                );
 
-                        trapFocus(mdialog);
-                    }
-                })
-            .catch(error => console.error('Error fetching data:', error));
-                //new stuff here
+                            trapFocus(mdialog);
+                        }
+                    })
+                .catch(error => console.error('Error fetching data:', error));
+                    //new stuff here
+            } else {
+
+
+                /*
+                * Only the first tab encountered with published
+                * figures receives true.
+                */
+                const is_first_tab_with_figures = true;
+
+                console.log('is_first_tab_with_figures', is_first_tab_with_figures);
+
+                create_tabs(
+                    i,
+                    tab_key,
+                    tab_title,
+                    title,
+                    modal_id,
+                    is_first_tab_with_figures
+                );
+
+                if (i === num_tabs) {
+                    let mdialog =
+                        document.querySelector(
+                            "#myModal > div"
+                        );
+
+                    trapFocus(mdialog);
+                }
+
+            }
         }
 
         // Google Tags
