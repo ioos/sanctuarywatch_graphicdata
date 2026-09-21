2dd9dfe5c12302d8521410b28079cfd9705f2922
Robbie Carroll <47251604+robbiecarroll@users.noreply.github.com>
2026-09-21 15:46:58 -0400

    #497 updates to block functionality

--- plugins/graphic_data_plugin/blocks/insert-figure/build/index.asset.php
+++ plugins/graphic_data_plugin/blocks/insert-figure/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e253d87e10382ba6a55e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '75c8030b17c417326532');
--- plugins/graphic_data_plugin/blocks/insert-figure/build/view.asset.php
+++ plugins/graphic_data_plugin/blocks/insert-figure/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch'), 'version' => '9cf8be4d4abda6f06eb7');
+<?php return array('dependencies' => array('wp-api-fetch'), 'version' => '296d2dbf652e4aa3e1ec');
